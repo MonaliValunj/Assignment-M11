cdf815e32627194c22797710288e787ef1727537
Monali Valunj <monavalunj96@gmail.com>
2024-01-10 15:24:06 -0800

    working on deathbyjs

--- js/deathbyjs.js
+++ js/deathbyjs.js
@@ -1,7 +1,25 @@
 //STEP 1
+// function sortAlphabeticaly(str){
+//     return str.split("").sort().join("")
+// } 
+// console.log(sortAlphabeticaly('webmaster'))
 
 //STEP 2
-
+/* function ConverToUppercase(str){
+    //contver string to array of words
+    var words = str.split(" ")
+    console.log(words)
+    for(var i = 0; i< words.length; i++){
+        //take the first char of the word convert it to uppercase 
+        //then concatenate uppcase letter to the rest of the word
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1)
+    }
+    // Join the words back into a string from array
+    var result = words.join(" ")
+    return result
+}
+console.log(ConverToUppercase('the quick brown fox'))
+ */
 //STEP 3
 
 //STEP 4
